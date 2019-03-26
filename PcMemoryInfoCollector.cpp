d4fe9c07794220a1ec02e299aa42ec55b03f9744
Sir0ga90 <1sergey99chursin0@gmail.com>
2019-03-27 01:08:22 +0200

    refactoring

--- PcMemoryInfoCollector.cpp
+++ PcMemoryInfoCollector.cpp
@@ -12,37 +12,32 @@ CPcMemoryInfoCollector::~CPcMemoryInfoCollector() {}
 
 
 void CPcMemoryInfoCollector::findAmountOfAllAvailableMemory() {
+    volatile char* p = nullptr;
+    uint64_t cntr = MIN_TEST_VAL;
 
-    constexpr uint64_t MAX_NUM_OF_BYTES = 100000000000;
-    char** memory_vector = new char*[0xffff];
-
-    for (uint64_t i = 0; i < MAX_NUM_OF_BYTES; ++i) {
-
-        try {
-            memory_vector[i] = new(char);
+    try
+    {
+        for(; cntr < MAX_TEST_VAL; ++cntr)
+        {
+            p = new char;
         }
-        catch (...) {
-            std::cout << "Exception: ";
-            return;
-        }
-
-        amountOfAllAvailableMemory = i;
     }
-
+    catch (std::exception& e)
+    {
+        amountOfAllAvailableMemory = cntr;
+        delete p;
+    }
 }
 
 
 void CPcMemoryInfoCollector::findMaxSizeOfSingleBlockOnHeap() {
-    char* p = nullptr;
-    constexpr uint64_t TEST_VAL = 15000000000;
-    uint64_t cntr = TEST_VAL;
+    volatile char* p = nullptr;
+    uint64_t cntr = MIN_TEST_VAL;
 
     try {
 
-        for (; cntr < 150000000000; cntr+=100000) {
-            p = new char[cntr];
-
-            p[cntr-1] = 34;
+        for (; cntr < MAX_TEST_VAL; cntr+=10000000) {
+            p = new (char[cntr]);
 
             std::cout << p[cntr-1];
 
@@ -50,19 +45,14 @@ void CPcMemoryInfoCollector::findMaxSizeOfSingleBlockOnHeap() {
         }
 
     }
-    catch (...)
+    catch (std::exception& e)
     {
-        std::cout << "Exception: ";
         maxSizeOfSingleBlock = cntr;
     }
 }
 
-void CPcMemoryInfoCollector::calculateMetricTime() {
-
-}
-
 void CPcMemoryInfoCollector::collectAndPrintData() {
-    //findAmountOfAllAvailableMemory();
+    findAmountOfAllAvailableMemory();
     findMaxSizeOfSingleBlockOnHeap();
 
     std::cout << "Total amount: ~" << amountOfAllAvailableMemory << "\n"
--- PcMemoryInfoCollector.h
+++ PcMemoryInfoCollector.h
@@ -9,10 +9,12 @@ public:
 
     void findAmountOfAllAvailableMemory();
     void findMaxSizeOfSingleBlockOnHeap();
-    void calculateMetricTime();
     void collectAndPrintData();
 
 private:
+    static constexpr uint64_t MIN_TEST_VAL = 15000000000;
+    static constexpr  uint64_t MAX_TEST_VAL = 100000000000;
+
     uint64_t amountOfAllAvailableMemory;
     uint64_t maxSizeOfSingleBlock;
 
